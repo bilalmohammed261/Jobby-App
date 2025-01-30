76f263ef75f173ba06f0e2f43d9f657d45355b58
Bilal <bilalmohammed261@gmail.com>
2025-01-30 10:44:37 +0000

    added login functionality

---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="home-container">
+      <h1>Find The Job That Fits Your Life</h1>
+      <p>
+        Millions of people are searching for jobs,salary,information,company
+        reviews.Find the job that fits your abilities and potential.
+      </p>
+      <button type="button">Find Jobs</button>
+    </div>
+  </>
+)
+
+export default Home
---
+++ src/components/Jobs/index.js
@@ -0,0 +1,10 @@
+import Header from '../Header'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <h1>Jobs Page</h1>
+  </>
+)
+
+export default Jobs
